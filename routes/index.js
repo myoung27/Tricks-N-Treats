395f236750c5bca204aab185cdc5986c859dea77
Myron Young <myronyoung@Myrons-MacBook-Pro.local>
2018-10-26 11:21:51 -0400

    edited index js within routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
+const userController = require('../controllers/users')
+const neighborhoodsController = require('../controllers/neighborhoods')
+const housesController = require('../controllers/houses')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
 
-module.exports = router;
+//read routes 
+router.get('/', userController.index)
+router.get('/stores', neighborhoodsController.index)
+router.get('/stores/new', neighborhoodsController.new)
+router.get('/stores/:storesId/edit', neighborhoodsController.edit)
+router.get('/stores/:storesId', neighborhoodsController.show)
+router.get('/stores/:storesId/products', housesController.index)
+router.get('/stores/:storesId/products/new', housesController.new)
+router.get('/stores/:storesId/products/:productId', housesController.show)
+router.get('/stores/:storesId/products/:productId/edit', housesController.edit)
+
+//create routes
+router.post('/stores', neighborhoodsController.create)
+router.post('/stores/:storesId/products', housesController.create)
+
+//delete routes 
+router.delete('/stores/:storesId', neighborhoodsController.delete)
+router.delete('/stores/:storesId/products/:productId', housesController.delete)
+
+//update routes 
+router.patch('/stores/:storesId', neighborhoodsController.update)
+router.patch('/stores/:storesId/products/:productId', housesController.update)
+
+module.exports = router
