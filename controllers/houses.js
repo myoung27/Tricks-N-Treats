4fc8f436cff3100b79279828c360c1362db2b152
Myron Young <myronyoung@Myrons-MacBook-Pro.local>
2018-10-26 11:24:56 -0400

    edited house js within controllers

--- controllers/houses.js
+++ controllers/houses.js
@@ -1,27 +1,10 @@
+const neighborhoods = require('../models/Neighborhood')
+const houses = require('../models/Houses')
 
-const Store = require('../models/Store')
-const Product = require('../models/Product')
-
-const storesController = {
+const userController = {
     index: (req, res) => {
-        Store.find({}).populate('products')
-            .then((stores) => {
-                res.render('stores/index', {
-                    stores: stores
-
-                })
-            })
-    },
-    new: (req, res) => {
-        res.send("Hey this is new route on stores")
-    },
-    show: (req, res) => {
-        const storeId = req.params.storesId
-        Store.findById(storeId).populate('products')
-            .then((store) => {
-                res.render('stores/show', {store: store})
-            })
+        res.send("Hey whats up this is app index")
     }
 }
 
-module.exports = storesController
+module.exports = userController
